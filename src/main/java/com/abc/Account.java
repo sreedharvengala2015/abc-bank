0e9aef6ff019171f8a19b5fe27b7c9009cd73f59
sreedharvengala <vpsree@hotmail.com>
2015-05-18 15:38:04 -0400

    Update Account.java
    Added new method to determine if there is a withdraw in the last 10day for Max_Savings interest calculation. Added additional comments and place holder for calculating daily interest and also daily balance for a duration.

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -8,6 +8,7 @@ public class Account {
     public static final int CHECKING = 0;
     public static final int SAVINGS = 1;
     public static final int MAXI_SAVINGS = 2;
+    public static final int MAXI_SAVINGS_CUTOFF_DAYS = 10;
 
     private final int accountType;
     public List<Transaction> transactions;
@@ -32,7 +33,8 @@ public void withdraw(double amount) {
         transactions.add(new Transaction(-amount));
     }
 }
-
+// Need to refactor to calculate interest per day. Need to calculate net balance
+// for each day to calculate interest per day
     public double interestEarned() {
         double amount = sumTransactions();
         switch(accountType){
@@ -41,20 +43,46 @@ public void withdraw(double amount) {
                     return amount * 0.001;
                 else
                     return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
             case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
+            	if (foundWithdrawBasedOnNumberOfDays(MAXI_SAVINGS_CUTOFF_DAYS))
+            		return amount * .001;
+            	else
+            		return amount * .05;
             default:
                 return amount * 0.001;
         }
     }
-
+/*    public double interestEarned() {
+        double amount = sumTransactions();
+        interest = 0.0;
+        switch(accountType){
+            case SAVINGS:
+                if (amount <= 1000)
+                    interest = amount * 0.001;
+                else
+                    interest = 1 + (amount-1000) * 0.002;
+            case MAXI_SAVINGS:
+            	if (foundWithdrawBasedOnNumberOfDays(MAXI_SAVINGS_CUTOFF_DAYS))
+            		interest = amount * .001;
+            	else
+            		interest = amount * .05;
+            default:
+                interest = amount * 0.001;
+        }
+        return interest;
+    }
+*/
+// TODO: The purpose of this method is to calculate daily interest rate per 
+// duration give start date and end date by utilizing the new method 
+// getDailyBalance (Date date)
+    public double interestEarnedPerDuration(Date fromDate, Date toDate){
+    	return 0.0;
+    }
+ // TODO: this method should be implemented to calculate daily interest. 
+ // Need more requirements.
+     public double getDailyBalance(Date date){
+     	return 0.0;
+     }
     public double sumTransactions() {
        return checkIfTransactionsExist(true);
     }
@@ -65,7 +93,18 @@ public void withdraw(double amount) {
             amount += t.amount;
         return amount;
     }
-
+    
+    private boolean foundWithdrawBasedOnNumberOfDays(int days) {
+    	Calendar beginDate = Calendar.getInstance();
+    	beginDate.add(Calendar.DATE, -days);
+    	for (Transaction t: transactions){
+    		if (t.transactionDate.compareTo(beginDate.getTime()) >=0  && t.amount < 0)
+    			return true;
+    		if (t.transactionDate.compareTo(beginDate.getTime()) < 0 )
+    			return false;
+    	}
+    	return false;
+    }
     public int getAccountType() {
         return accountType;
     }
